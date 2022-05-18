2bc9721e60a0b09edd9fa02414e5720e0a90c4f1
Nicolas Epstein <nicolas.epstein@qc.croixbleue.ca>
2022-05-18 16:12:51 -0400

    add provisional Cache class

--- devops_sccs/cache.py
+++ devops_sccs/cache.py
@@ -14,30 +14,44 @@
 
 # You should have received a copy of the GNU Lesser General Public License
 # along with python-devops-sccs.  If not, see <https://www.gnu.org/licenses/>.
+from collections import UserDict
 from multiprocessing import managers
 import logging
 import multiprocessing
 import threading
 import traceback
 
+
+class Cache(UserDict):
+    """A simple cache to help reduce expensive API calls."""
+
+
 class AsyncCache(object):
-    def __init__ (self ,data ,lookup_func,key_arg:str=None,rlock = multiprocessing.RLock(), **kwargs_func):
+    def __init__(
+        self,
+        data,
+        lookup_func,
+        key_arg: str = None,
+        rlock=multiprocessing.RLock(),
+        **kwargs_func,
+    ):
         """
         loopup_func:async   callable  function to call when a key is not found in the cache
-        key_arg:string      name of the key argument 
+        key_arg:string      name of the key argument
         rlock:Rlock         recursive lock for writing (needs a context manager)
         data:dict           initial data of the cache
         kwargs_func         arguments to call with lookup_func
         """
-        
+
         self.data = data
-        #setup lookup function
+        # setup lookup function
         self.lookup_func = lookup_func
         self.key_arg = key_arg
-        self.kwargs_func =  kwargs_func
-        self.rlock = rlock 
+        self.kwargs_func = kwargs_func
+        self.rlock = rlock
         print(f"new cache at : {self.data.__repr__()}")
-    def get(self,key):
+
+    def get(self, key):
         return self.data.get(key)
 
     def clear_cache(self):
@@ -45,49 +59,51 @@ class AsyncCache(object):
 
     async def __getitem__(self, key):
         val = self.data.get(key)
-        if(val is None):
-        
+        if val is None:
+
             if self.lookup_func is not None:
                 with self.rlock as lock:
-                    #check if the data is still unitialized
-                    val = self.data.get(key)                    
-                    if (val is None) :
-                        #data is definetly unitialized
-                        print(f"element {key} not found in the cache at {self.data.__repr__()}! populating it!")
+                    # check if the data is still unitialized
+                    val = self.data.get(key)
+                    if val is None:
+                        # data is definetly unitialized
+                        print(
+                            f"element {key} not found in the cache at {self.data.__repr__()}! populating it!"
+                        )
                         self.kwargs_func[self.key_arg] = key
                         val = await self.lookup_func(**self.kwargs_func)
-                        self.data[key]=val
-                        
-        
-            else :
-        
+                        self.data[key] = val
+
+            else:
+
                 raise KeyError(key)
-                    
+
         return val
 
-    def __setitem__(self, key, item) :
-        with self.rlock as lock :
+    def __setitem__(self, key, item):
+        with self.rlock as lock:
             print(f"key {key} has been set at {self.data.__repr__()} !")
-            self.data[key]=item
+            self.data[key] = item
 
     def __enter__(self):
         self.rlock.acquire()
         return self.data
-    
-    def __exit__(self,type, value, traceback):
+
+    def __exit__(self, type, value, traceback):
         self.rlock.release()
 
-    def __new__(cls,*args,**kwargs) :
+    def __new__(cls, *args, **kwargs):
         logging.debug("new cache!")
         return super().__new__(cls)
 
-    def __getstate__(self) :
+    def __getstate__(self):
         logging.debug("get state cache")
         return self.__dict__
-    
-    def __setstate__(self,state):
+
+    def __setstate__(self, state):
         logging.debug("set state cache")
         self.__dict__ = state
+
     def __copy__(self):
         logging.debug("copy cache!")
         self.__dict__.copy()
