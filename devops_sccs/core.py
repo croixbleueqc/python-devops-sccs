b84c1d193d8145e13137ad5c034fb92cd13afee2
Nicolas Epstein <nicolas.epstein@qc.croixbleue.ca>
2022-05-20 18:57:39 -0400

    comment out hook logic

--- devops_sccs/core.py
+++ devops_sccs/core.py
@@ -36,13 +36,14 @@ from .realtime.scheduler import Scheduler
 # Built-in plugins
 from .plugins import demo as plugin_demo
 
+
 class Core(object):
-    
+
     """
     Manages source code control systems
     Manages a standard workflow to use a specific source code control system
     """
-    
+
     class ControlledContext:
         """Create/Delete context in a with statement"""
 
@@ -60,49 +61,54 @@ class Core(object):
 
     def __init__(self):
         """Initialize plugins and internal modules"""
+
         self.plugins = {}
         self.scheduler = Scheduler()
+        self.provision: Provision
 
     @classmethod
-    async def create(cls, config={}):
+    async def create(cls, config=None):
+        if config is None:
+            config = {}
         self = Core()
         if config.get("provision") is not None:
             self.provision = Provision(
                 config["provision"].get("checkout_base_path", "/tmp"),
                 main=config["provision"]["main"],
                 repository=config["provision"].get("repository", {}),
-                templates=config["provision"].get("templates", {})
-                )
+                templates=config["provision"].get("templates", {}),
+            )
 
-        self.enableHook = config.get("hook_server") is not None
-        if self.enableHook :
-            self.hookServer = HookServer(config["hook_server"])
-            #we need to lock it up here , otherwise the endpoints won't register.
-            await self.hookServer.lock.acquire()   
+        # self.enableHook = config.get("hook_server") is not None
+        # if self.enableHook:
+        #     self.hookServer = HookServer(config["hook_server"])
+        #     # we need to lock it up here , otherwise the endpoints won't register.
+        #     await self.hookServer.lock.acquire()
+
         val = await self.load_builtin_plugins(config.get("plugins", {}))
 
         val = await self.load_external_plugins(config.get("plugins", {}))
-        
-        if self.enableHook and val :
 
-            self.hookServer.lock.release()
-            await self.hookServer.start_server()
-
+        # if self.enableHook and val:
+        #
+        #     self.hookServer.lock.release()
+        #     await self.hookServer.start_server()
+        #
         return self
 
     async def cleanup(self):
         if self.provision is not None:
             self.provision.cleanup()
 
-        if self.enableHook :
-            self.hookServer.stop_server()
-
+        # if self.enableHook:
+        #     self.hookServer.stop_server()
+        #
         for plugin_id in list(self.plugins.keys()):
             await self.unregister(plugin_id)
 
     async def load_builtin_plugins(self, plugins_config):
         """Built-in plugins
-        
+
         A config can be passed to skip/or config some built-in plugins.
         By default all built-in plugins will be loaded
         """
@@ -112,35 +118,38 @@ class Core(object):
         if builtin.get("demo", True):
             plugin_id, plugin = plugin_demo.init_plugin()
             await self.register(plugin_id, plugin, config.get("demo"))
-        
+
         return True
 
     async def load_external_plugins(self, plugins_config):
         """External plugins
-        
+
         All files with .py extension will be loaded
         """
 
         external_path = plugins_config.get("external")
         config = plugins_config.get("config", {})
-        
+
         if os.path.isdir(external_path):
             sys.path.append(external_path)
             await self.register_plugins_in_folder(external_path, config)
         else:
-            dev_external_path=os.getcwd() + "/plugins/devops_sccs"
+            dev_external_path = f"{os.getcwd()}/plugins/devops_sccs"
             if os.path.isdir(dev_external_path):
                 sys.path.append(dev_external_path)
                 await self.register_plugins_in_folder(dev_external_path, config)
-        
-                
+
         return True
-    
+
     async def register_plugins_in_folder(self, folder_path, config):
         for file in glob.glob(os.path.join(folder_path, "*.py")):
             spec = importlib.util.spec_from_file_location("", file)
+            if spec is None:
+                raise ModuleNotFoundError("something went wrong loading the plugin")
             mod = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(mod)
+
+            if spec.loader is not None:
+                spec.loader.exec_module(mod)
 
             plugin_id, plugin = mod.init_plugin()
             await self.register(plugin_id, plugin, config.get(plugin_id))
@@ -149,18 +158,17 @@ class Core(object):
         """Register a plugin to make it available"""
         if plugin_id in self.plugins:
             raise PluginAlreadyRegistered(plugin_id)
-        
+
         await plugin.init(self, config)
         self.plugins[plugin_id] = plugin
-        
 
     async def unregister(self, plugin_id):
         plugin = self.plugins.pop(plugin_id)
         await plugin.cleanup()
-    
+
     async def create_context(self, plugin_id, args):
         """Create a context
-        
+
         A session and the real plugin will be embedded in a context.
 
         This is the main entry point to communicate with the sccs
@@ -168,7 +176,7 @@ class Core(object):
         plugin = self.plugins.get(plugin_id)
         if plugin is None:
             raise PluginNotRegistered(plugin_id)
-        
+
         session_id = plugin.get_session_id(args)
 
         session = await plugin.open_session(session_id, args)
@@ -178,11 +186,7 @@ class Core(object):
     async def delete_context(self, context, args=None):
         """Delete a context by closing a session"""
 
-        await context.plugin.close_session(
-            context.session_id,
-            context.session,
-            args
-        )
+        await context.plugin.close_session(context.session_id, context.session, args)
 
     def context(self, plugin_id, args):
         """Controlled context to use in a with statement"""
